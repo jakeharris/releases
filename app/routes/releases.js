936a351d5b2953274c4529a8c1e493cfed20c91b
Jake Harris <milieu@javakat.io>
2015-08-19 13:13:45 -0500

    Add model function

--- app/routes/releases.js
+++ app/routes/releases.js
@@ -1,3 +1,8 @@
 export default Ember.Routes.extend ({
-  
-});
+  model: function (params) {
+    return ic.ajax({
+      url: 'https://api.github.com/repos/' + params.owner + '/' + params.repo + '/' + 'releases',
+      type: 'get'
+    })
+  }
+})
